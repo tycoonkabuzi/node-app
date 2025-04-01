41db26fb15f00ecba78e30c129fe189fa50928bf
Tycoon <tycoon@Tycoons-MacBook-Pro.local>
2025-04-01 21:24:25 +0200

    First part Stream

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 // // console.log(math.multiply(1, 2, 4, 3));
 const fs = require("fs");
 const path = require("path");
+const EventEmitter = require("events");
 // fs.readdir(path.join(__dirname, "data"), function (err, files) {
 //   // locating the folder where the files are , base on that we see all the files in the folder
 //   if (err) {
@@ -110,36 +111,40 @@ const path = require("path");
 // UTILISATION OF EVENTS IN NODE.
 // events are another one of the core modules in Node, and thy are so significannt that it's worth discussing them seperately.
 
-const EventEmitter = require("events");
 // const emitter = new EventEmitter();
 // emitter.on("message", (msg) => {
 //   console.log("Message :" + msg);
 // });
 // emitter.emit("message", "Hello World");
 // emitter.emit("message", "World");
+// const printdata = (data) => {
+//   console.log(`Data ${data.data} from the address ${data.url}`);
+// };
+// class Puller extends EventEmitter {
+//   constructor(url) {
+//     super();
+//     this.url = url;
+//   }
+//   pull() {
+//     this.interval = setInterval(() => {
+//       this.emit("getData", { data: "Taken data", url: this.url });
+//     }, 1000);
+//   }
 
-const printdata = (data) => {
-  console.log(`Data ${data.data} from the address ${data.url}`);
-};
-class Puller extends EventEmitter {
-  constructor(url) {
-    super();
-    this.url = url;
-  }
-  pull() {
-    this.interval = setInterval(() => {
-      this.emit("getData", { data: "Taken data", url: this.url });
-    }, 1000);
-  }
+//   stop(callback) {
+//     this.removeListener("getData", callback);
+//     clearInterval(this.interval);
+//   }
+// }
+// const puller = new Puller("https://example.com");
+// puller.on("getData", printdata);
+// puller.pull();
+// setTimeout(() => {
+//   puller.stop(printdata);
+// }, 10000);
+// STREAM IN NODE.
 
-  stop(callback) {
-    this.removeListener("getData", callback);
-    clearInterval(this.interval);
-  }
-}
-const puller = new Puller("https://example.com");
-puller.on("getData", printdata);
-puller.pull();
-setTimeout(() => {
-  puller.stop(printdata);
-}, 10000);
+let readStream = fs.createReadStream("./data-temp/studentami-first-year.txt");
+readStream.on("data", (chunk) => {
+  console.log(chunk.toString());
+});
