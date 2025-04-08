79713711ca4471e656dc67d1e071c73739cd138d
Tycoon <tycoon@Tycoons-MacBook-Pro.local>
2025-04-08 16:39:02 +0200

    project done

--- 2-read-write/index.js
+++ 2-read-write/index.js
@@ -1,7 +1,9 @@
 const path = require("path");
 const fs = require("fs");
 
-const saveData = (pathToJason, newFolder, isExisting) => {
+const saveData = (pathToJason, newFolder, isPresent) => {
+  // created a counter to have the folder name changed accordingly
+  let counter = 0;
   // reading the directory where the file is
   fs.readdir(path.join(pathToJason), (err, file) => {
     if (err) {
@@ -16,52 +18,56 @@ const saveData = (pathToJason, newFolder, isExisting) => {
           } else {
             //create a folder with the name provided as an argument
 
-            fs.mkdir(path.join(__dirname, newFolder), (err) => {
-              if (err) {
-                if (err.code == "EEXIST") {
-                  isExisting == true;
-                } else {
-                  console.log(err);
-                  isExisting == false;
-                }
-              } else {
-                if (isExisting) {
-                  console.log(`There is a folder with name ${newFolder}`);
+            const createFolderAndAddingFiles = (folderName) => {
+              fs.mkdir(path.join(__dirname, folderName), (err) => {
+                if (err && err.code === "EEXIST") {
+                  if (isPresent) {
+                    counter++;
+                    createFolderAndAddingFiles(`${newFolder} ${counter}`);
+                  } else {
+                    console.log("This folder is already created");
+                  }
+                } else if (err) {
+                  console.log(
+                    "We have this error when creating the folder" + err
+                  );
                 } else {
                   console.log(
-                    `The folder ${newFolder} was created successfully`
+                    `The folder ${folderName} was created successfully`
                   );
-                  const jsoData = JSON.parse(data);
-                  jsoData.forEach((user) => {
-                    fs.writeFile(
-                      path.join(
-                        __dirname,
-                        newFolder,
-                        `${user.id}-${user.name}.txt`
-                      ),
-                      `name: ${user.name}
-                    street: ${user.address.street}
-                    Zip Code:${user.address.zipcode}
-                    city:${user.address.city}
-                    phone: ${user.phone}`,
-                      (err) => {
-                        if (err) {
-                          console.log(err);
-                        } else {
-                          console.log(
-                            `The file ${user.id}-${user.name}.txt has been created`
-                          );
-                        }
-                      }
-                    );
-                  });
                 }
-              }
-            });
+                const jsoData = JSON.parse(data);
+                jsoData.forEach((user) => {
+                  fs.writeFile(
+                    // writing in the file. adding bellow information
+                    path.join(
+                      __dirname,
+                      folderName,
+                      `${user.id}-${user.name}.txt`
+                    ),
+                    `name: ${user.name}
+                street: ${user.address.street}
+                Zip Code:${user.address.zipcode}
+                city:${user.address.city}
+                phone: ${user.phone}`,
+                    (err) => {
+                      if (err) {
+                        console.log(err);
+                      } else {
+                        console.log(
+                          `The file ${user.id}-${user.name}.txt has been created`
+                        );
+                      }
+                    }
+                  );
+                });
+              });
+            };
+            createFolderAndAddingFiles(newFolder);
           }
         }
       );
     }
   });
 };
-saveData("./json", "main", false);
+saveData("./json", "main", true);
